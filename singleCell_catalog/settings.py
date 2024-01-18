3958d3f1fb73fb0c8a8588f740f034ebb0c3471b
Clement Helsens <clementhelsens>
2024-01-18 15:38:12 +0100

    add rd db

--- singleCell_catalog/settings.py
+++ singleCell_catalog/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#VMachine
+if os.path.isdir('/home/helsens/Software/segmentationTools/cellgmenter/main'):
+    sys.path.append('/home/helsens/Software/UPOATES_catalog')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
