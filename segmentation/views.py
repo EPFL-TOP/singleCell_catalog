2ebed3effa42a8f6168d50a6720aa82353fe48dd
Clement Helsens <clementhelsens>
2024-02-01 15:46:31 +0100

    change the model

--- segmentation/views.py
+++ segmentation/views.py
@@ -333,6 +333,7 @@ def build_cells():
                 for f in frames:
                     cellf = CellFrame.objects.select_related().filter(frame=f)
                     cell_frame_list.append(cellf)
+                    print(cellf)
                     print(cellf.frame.sample.file_name)
                 print('number of cell frames=',len(cell_frame_list))
 
