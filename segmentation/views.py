262bc0e95d6f5124a69279453d1dea641977b8f7
Clement Helsens <clementhelsens>
2024-02-02 09:46:38 +0100

    change the model

--- segmentation/views.py
+++ segmentation/views.py
@@ -424,11 +424,10 @@ def intensity():
                 for cid in cellsid:
                     print('            ---- INTENSITY cellid name ',cid.name)
                     cell_frames = CellFrame.objects.select_related().filter(cell_id=cid)
-                    frames      = CellFrame.objects.select_related("frame").distinct()
-                    frames      = CellFrame.objects.select_related("frame").prefetch_related("frame")
+                    frames      = []
                     for cf in cell_frames:
-                        frame = cf.frame
-                        print(frame)
+                        frames.append(cf.frame)
+
                     print('n frames=',len(frames),'   n cell_frames=',len(cell_frames))
 
 
