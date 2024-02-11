c43fb32f26461c58b316a38be3931ad7fd4771ba
Clement Helsens <clementhelsens>
2024-02-11 17:54:25 +0100

    generic update

--- segmentation/views.py
+++ segmentation/views.py
@@ -726,8 +726,7 @@ def index(request):
     
 
     #create a plot
-    plot = figure(plot_width=400, plot_height=400)
- 
+    plot = figure(width=400)
     # add a circle renderer with a size, color, and alpha
     plot.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
     script, div = components(plot)
