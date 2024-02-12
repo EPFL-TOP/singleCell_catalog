eba5a0640acdfa1ca22efd161573804452505d79
Clement Helsens <clementhelsens>
2024-02-12 16:19:27 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -839,6 +839,7 @@ def index(request):
     //console.log(data['img']);
     const img = data['img2'];
     //data= ind_images[time_point];
+    source.data = {'img':[data['img2']]};
     source.change.emit(); 
     """) 
 
