2d532623e1de7a6d193d90ebb121db94c7bc6c87
Clement Helsens <clementhelsens>
2024-02-12 20:41:58 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -803,6 +803,7 @@ def index(request):
     time_lapse_path = Path('/mnt/nas_rcp/raw_data/microscopy/cell_culture/wscepfl0060_well1/raw_files/wsc_epfl-wscl_060_xy01.nd2')
     time_lapse = nd2.imread(time_lapse_path.as_posix())
     time_lapse = time_lapse[:,bf_channel,:,:] # Assume I(t, c, x, y)
+    time_lapse=np.uint8(time_lapse)
 
     time_domain = np.asarray(np.linspace(0, time_lapse.shape[0] - 1, time_lapse.shape[0]), dtype=np.uint)
     ind_images = [time_lapse[i,:,:] for i in time_domain]
