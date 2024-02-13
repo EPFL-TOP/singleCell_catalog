4ef400367a72766ea126c2895e92c0aad8dfb72d
Clement Helsens <clementhelsens>
2024-02-13 11:22:26 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -779,6 +779,7 @@ def index(request):
         im = p.image(image='img', x=0, y=0, dw=time_lapse.shape[1], dh=time_lapse.shape[2],source=source, palette='Greys256')
         for roi in rois:
             p.rect(roi.min_col, 512-roi.min_row, roi.max_col-roi.min_col, 512-(roi.max_col-roi.min_col), line_width=2, fill_alpha=0, line_color="white") 
+            print('roi.min_col=',roi.min_col, '  roi.max_col=',roi.max_col,'  roi.min_row=',roi.min_row,'  roi.max_row=',roi.max_row)
         # Remove the axes
         p.axis.visible = False
         p.grid.visible = False
