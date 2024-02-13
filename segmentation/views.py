fa2a12eaa0f328b595c9ac87fae91b46874fc620
Clement Helsens <clementhelsens>
2024-02-13 11:52:20 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -783,6 +783,38 @@ def index(request):
         # Remove the axes
         p.axis.visible = False
         p.grid.visible = False
+
+
+        #custom rROI
+        source_roi = bokeh.models.ColumnDataSource(data=dict(left=[], right=[], top=[], bottom=[]))
+
+        callback_roi = bokeh.models.CustomJS(args=dict(source=source), code="""
+            const geometry = cb_obj.geometry
+            const data = source.data
+
+            // quad is forgiving if left/right or top/bottom are swappeed
+            source.data = {
+                left: data.left.concat([geometry.x0]),
+                right: data.right.concat([geometry.x1]),
+                top: data.top.concat([geometry.y0]),
+                bottom: data.bottom.concat([geometry.y1])
+            }
+        """)
+
+        #p = figure(width=400, height=400, title="Select below to draw rectangles",
+        #        tools="box_select", x_range=(0, 1), y_range=(0, 1))
+
+        # using Quad model directly to control (non)selection glyphs more carefully
+        quad = bokeh.models.Quad(left='left', right='right',top='top', bottom='bottom',
+                    fill_alpha=0.3, fill_color='#009933')
+
+        p.add_glyph(source, quad, selection_glyph=quad, nonselection_glyph=quad)
+
+        p.js_on_event(bokeh.events.SelectionGeometry, callback)
+
+
+
+
         norm_layout = bokeh.layouts.row(
             p,
             bokeh.layouts.Spacer(width=15),
