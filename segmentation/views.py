afb6ca04fe9b18f8eedd5cd521b5270aa787d941
Clement Helsens <clementhelsens>
2024-02-16 18:52:16 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -636,13 +636,17 @@ def segmentation_handler(doc: bokeh.document.Document ) -> None:
     def callback_roi(event):
         if isinstance(event, SelectionGeometry):
             print(source_roi.data)
-            source_roi.data = {
-            "left": [event.geometry['x0']],
-            "right": [event.geometry['x1']],
-            "top": [event.geometry['y0']],
-            "bottom": [event.geometry['y1']]
-            }
+            #source_roi.data = {
+            #"left": [event.geometry['x0']],
+            #"right": [event.geometry['x1']],
+            #"top": [event.geometry['y0']],
+            #"bottom": [event.geometry['y1']]
+            #}
 
+            source_roi.data["left"].append(event.geometry['x0'])
+            source_roi.data["right"].append(event.geometry['x1'])
+            source_roi.data["top"].append(event.geometry['y0'])
+            source_roi.data["bottom"].append(event.geometry['y1'])
             for exp in Experiment.objects.all():
                 print(' ---- Experiment name in  callback_roi ',exp.name)
                 experimentaldataset = ExperimentalDataset.objects.select_related().filter(experiment = exp)
