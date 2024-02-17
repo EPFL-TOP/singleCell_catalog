3e95529f4565675e8cd890af6816255df44f8772
Clement Helsens <clementhelsens>
2024-02-17 12:13:14 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -763,6 +763,9 @@ def segmentation_handler(doc: bokeh.document.Document ) -> None:
     button_prev.on_click(prev_callback)
 
 
+    # Create a Div widget with some text
+    text = bokeh.models.Div(text="<h1>This is some text next to the plot</h1>")
+
 
     left_rois,right_rois,right_rois,bottom_rois=update_source_roi()
     source_roi    = bokeh.models.ColumnDataSource(data=dict(left=left_rois, right=right_rois, top=top_rois, bottom=bottom_rois))
@@ -781,7 +784,7 @@ def segmentation_handler(doc: bokeh.document.Document ) -> None:
     p.grid.visible = False
 
     slider_layout = bokeh.layouts.column(bokeh.layouts.Spacer(height=10), slider)
-    norm_layout = bokeh.layouts.column(bokeh.layouts.row(p), 
+    norm_layout = bokeh.layouts.column(bokeh.layouts.row(p), text,
                                        bokeh.layouts.row(slider_layout,button_play_stop, button_prev, button_next ),
                                        bokeh.layouts.row(button_delete_roi, button_save_roi, dropdown_time ))
 
