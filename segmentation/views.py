e3e30b9d1f749c2172bf7ba89579727ca5ef49e2
Clement Helsens <clementhelsens>
2024-02-17 12:19:26 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -784,9 +784,15 @@ def segmentation_handler(doc: bokeh.document.Document ) -> None:
     p.grid.visible = False
 
     slider_layout = bokeh.layouts.column(bokeh.layouts.Spacer(height=10), slider)
-    norm_layout = bokeh.layouts.column(bokeh.layouts.row(p), [text,
+
+    right_col = bokeh.layouts.column( text,
                                        bokeh.layouts.row(slider_layout,button_play_stop, button_prev, button_next ),
-                                       bokeh.layouts.row(button_delete_roi, button_save_roi, dropdown_time )])
+                                       bokeh.layouts.row(button_delete_roi, button_save_roi, dropdown_time ))
+    
+    norm_layout = bokeh.layouts.row(p, right_col)
+    #norm_layout = bokeh.layouts.column(bokeh.layouts.row(p), text,
+    #                                   bokeh.layouts.row(slider_layout,button_play_stop, button_prev, button_next ),
+    #                                   bokeh.layouts.row(button_delete_roi, button_save_roi, dropdown_time ))
 
     doc.add_root(norm_layout)
 
