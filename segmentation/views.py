ce54d47544469f167cf21ddf55ec80496ae13463
Clement Helsens <clementhelsens>
2024-02-18 20:03:50 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -674,7 +674,7 @@ def segmentation_handler(doc: bokeh.document.Document ) -> None:
         for roi in rois:
             weight.append(roi.min_col+(roi.max_col - roi.min_col)/2.)
             #height.append(roi.min_row+(roi.max_row - roi.min_row)/2.)
-            height.append(roi.min_row)
+            height.append(roi.max_row)
             names.append('CellROI {0}'.format(roi.roi_number))
         print('ppppppp ',height, weight, names)
         return height, weight, names
@@ -809,7 +809,7 @@ def segmentation_handler(doc: bokeh.document.Document ) -> None:
     print('---ffeefefefe -   ',height, weight, names)
     source_labels = bokeh.models.ColumnDataSource(data=dict(height=height,weight=weight,names=names))
     labels = bokeh.models.LabelSet(x='weight', y='height', text='names', x_units='data', y_units='data',
-                  x_offset=0, y_offset=5, source=source_labels, text_color='white', text_font_size="12pt")
+                  x_offset=0, y_offset=0, source=source_labels, text_color='white', text_font_size="12pt")
 
     p.add_layout(labels)
     # Create Bokeh figure and use image display
