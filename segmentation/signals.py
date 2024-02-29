74081b2b3947f74285e26724db4d95817d80f5eb
Clement Helsens <clementhelsens>
2024-02-29 11:34:11 +0100

    generic update commit

--- segmentation/signals.py
+++ segmentation/signals.py
@@ -5,21 +5,21 @@ from segmentation.models import Contour
 
 from django.dispatch import receiver
 
-@receiver(post_save,sender=Contour)
-def post_create_contour(sender,instance,created,**kwargs):
-    if created:
-        #write your logic here
-        print("CONTOUR Created")
+#@receiver(post_save,sender=Contour)
+#def post_create_contour(sender,instance,created,**kwargs):
+#    if created:
+#        #write your logic here
+#        print("CONTOUR Created")
         
-@receiver(post_delete,sender=Contour)
-def post_delete_contour(sender,instance,*args,**kwargs):
-    #write your login when user profile is deleted.
-    print("CONTOUR Profile Deleted")
-    if instance.pixels_data_contour:
-        instance.pixels_data_contour.delete()
-        print("pixels_data_contour Deleted")
-
-    if instance.pixels_data_inside:
-        instance.pixels_data_inside.delete()
-        print("pixels_data_inside Deleted")
+#@receiver(post_delete,sender=Contour)
+#def post_delete_contour(sender,instance,*args,**kwargs):
+#    #write your login when user profile is deleted.
+#    print("CONTOUR Profile Deleted")
+#    if instance.pixels_data_contour:
+#        instance.pixels_data_contour.delete()
+#        print("pixels_data_contour Deleted")
+#
+#    if instance.pixels_data_inside:
+#        instance.pixels_data_inside.delete()
+#        print("pixels_data_inside Deleted")
 
