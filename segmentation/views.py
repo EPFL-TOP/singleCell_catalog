27e4e9763754b3d92cc507c8c8911b5ddaf0a677
Clement Helsens <clementhelsens>
2024-03-08 09:30:51 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -1422,6 +1422,9 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
     initial_position = source_intensity_ch1.data["time"][0]
     line_position = bokeh.models.Span(location=initial_position, dimension='height', line_color='red', line_width=2)
     plot_intensity.add_layout(line_position)
+    x_area = source_intensity_ch1.data["time"] + source_intensity_ch1.data["time"][::-1]
+    y_area = [0] + source_intensity_ch1.data["intensity"] + [0]
+    plot_intensity.varea(x=x_area, y1=y_area, y2=[0] * len(x_area), fill_alpha=0.2, fill_color='blue')
 
     # Add the rectangle glyph after adding the image
     quad = bokeh.models.Quad(left='left', right='right', top='top', bottom='bottom', fill_alpha=0.3, fill_color='#009933')
