9725742f3f446caf62c877929cb399dcddbee2de
Clement Helsens <clementhelsens>
2024-03-08 09:38:52 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -1418,16 +1418,15 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
 
     #plot_intensity.legend.labels = leg_label
 
-    print("source_intensity_ch1.data[time] ",source_intensity_ch1.data["time"])
     initial_position = source_intensity_ch1.data["time"][0]
     line_position = bokeh.models.Span(location=initial_position, dimension='height', line_color='red', line_width=2)
     plot_intensity.add_layout(line_position)
     x_area = source_intensity_ch1.data["time"] + source_intensity_ch1.data["time"][::-1]
     y_area = [0] + source_intensity_ch1.data["intensity"] + [0]
-    #plot_intensity.varea(x=x_area, y1=y_area, y2=[0] * len(x_area), fill_alpha=0.2, fill_color='blue')
-    band = bokeh.models.Band(base='time', upper='intensity', source=source_intensity_ch1, level='underlay',
-            fill_alpha=0.2, fill_color='#55FF88')
-    plot_intensity.add_layout(band)
+    plot_intensity.varea(x=source_intensity_ch1.data["time"], y1=source_intensity_ch1.data["intensity"], y2=source_intensity_ch1.data["time"]*0.8, fill_alpha=0.2, fill_color='blue')
+    #band = bokeh.models.Band(base='time', upper='intensity', source=source_intensity_ch1, level='underlay',
+    #        fill_alpha=0.2, fill_color='#55FF88')
+    #plot_intensity.add_layout(band)
 
 
     # Add the rectangle glyph after adding the image
