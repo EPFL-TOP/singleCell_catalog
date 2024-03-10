3d062997d5a6d5d9c8438f6ea79174399c1f4be0
Clement Helsens <clementhelsens>
2024-03-10 16:22:23 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -1783,6 +1783,7 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
             int_min.append(source_intensity_ch1.data["intensity"][p])
         source_intensity_min.data={'time':time_min, 'intensity':int_min}
         print('peaksmax=',peaksmax,'  peaksmin=',peaksmin)
+        set_rising_falling(peaksmax, peaksmin)
     slider_find_peaks.on_change('value', find_peaks_slider_callback)
     #___________________________________________________________________________________________
 
