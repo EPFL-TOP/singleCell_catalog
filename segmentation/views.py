dc9485dc8643f3bf3d9bea4327349086146d1756
Clement Helsens <clementhelsens>
2024-03-13 17:59:46 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -1136,9 +1136,9 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
 
         if slider.value == 0:
             print('in the if update_dropdown_pos')
-            left_rois,right_rois,top_rois,bottom_rois=update_source_roi()
-            height_labels, weight_labels, names_labels = update_source_labels_roi()
-            height_cells, weight_cells, names_cells = update_source_labels_cells()
+            left_rois,right_rois,top_rois,bottom_rois,height_labels, weight_labels, names_labels,height_cells, weight_cells, names_cells=update_source_roi_cell_labels()
+            #height_labels, weight_labels, names_labels = update_source_labels_roi()
+            #height_cells, weight_cells, names_cells = update_source_labels_cells()
         
             source_roi.data = {'left': left_rois, 'right': right_rois, 'top': top_rois, 'bottom': bottom_rois}
             source_labels.data = {'height':height_labels, 'weight':weight_labels, 'names':names_labels}
