174155aa304f1468b4821653f1960ace0682f4ca
Clement Helsens <clementhelsens>
2024-03-18 10:01:07 +0100

    generic update commit

--- segmentation/admin.py
+++ segmentation/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Sample, Frame, CellFlag, CellStatus, Contour, Experiment, ExperimentalDataset, Segmentation, SegmentationChannel, CellID, CellROI
 # Register your models here.
+
+class CellFlagAdmin(admin.ModelAdmin):
+    search_fields = ["cell_roi__frame__sample__file_name"]
+
 admin.site.register(Sample)
 admin.site.register(Frame)
-admin.site.register(CellFlag)
+admin.site.register(CellFlag, CellFlagAdmin)
 admin.site.register(CellStatus)
 admin.site.register(Contour)
 admin.site.register(Experiment)
