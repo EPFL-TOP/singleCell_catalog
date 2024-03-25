99863dfa7dcc05a2280ca5c5c1848f30cbdeb385
Clement Helsens <clementhelsens>
2024-03-25 13:46:03 +0100

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -2679,9 +2679,8 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
         for i in range(frame.height-cellROI.max_row, frame.height-cellROI.min_row):
             for j in range(cellROI.min_col, cellROI.max_col+1):
                 intlist.append(source_img_ch.data['img'][0][i][j])
-        hist, edges = np.histogram(intlist, bins=100, range=(min(intlist), max(intlist)))
+        hist, edges = np.histogram(intlist, bins=int((max(intlist)-min(intlist))/10.), range=(min(intlist), max(intlist)))
         source_histo_int.data={'x': edges[:-1], 'top': hist}
-        plot_histo_int.width=int(0.95*(max(intlist)-min(intlist))/100.)
     button_segment_cell = bokeh.models.Button(label="Segment cell")
     button_segment_cell.on_click(segment_cell_callback)
     #___________________________________________________________________________________________
@@ -2897,7 +2896,7 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
 
     plot_histo_int      = bokeh.plotting.figure(title="histo int", x_axis_label='intensity', y_axis_label='Number of pixels',width=400, height=300)
     source_histo_int      = bokeh.models.ColumnDataSource(data=dict(x=[], top=[]))
-    plot_histo_int.vbar(x='x', top='top', width=2., source=source_histo_int, alpha=0.2, color='green', line_color=None)
+    plot_histo_int.vbar(x='x', top='top', width=10., source=source_histo_int, alpha=0.2, color='green', line_color=None)
 
 
     # Sample data
