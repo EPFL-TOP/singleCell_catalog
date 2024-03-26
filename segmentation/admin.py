a6c9dfe888e006674c99744a30f941b042dfedd1
Clement Helsens <clementhelsens>
2024-03-26 16:04:09 +0100

    generic update commit

--- segmentation/admin.py
+++ segmentation/admin.py
@@ -5,12 +5,16 @@ from .models import Sample, Frame, CellFlag, CellStatus, Contour, Experiment, Ex
 class CellFlagAdmin(admin.ModelAdmin):
     search_fields = ["cell_roi__frame__sample__file_name"]
 
+
+class ContourSegAdmin(admin.ModelAdmin):
+    search_fields = ["cell_roi__frame__sample__file_name"]
+
 admin.site.register(Sample)
 admin.site.register(Frame)
 admin.site.register(CellFlag, CellFlagAdmin)
 admin.site.register(CellStatus)
 admin.site.register(Contour)
-admin.site.register(ContourSeg)
+admin.site.register(ContourSeg, ContourSegAdmin)
 admin.site.register(Experiment)
 admin.site.register(ExperimentalDataset)
 admin.site.register(CellID)
