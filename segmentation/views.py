24802e269013e2bb178d49a7071daa4f2fdfb2b0
Clement Helsens <clementhelsens>
2024-05-13 15:31:53 +0200

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -681,7 +681,7 @@ def build_ROIs_loop():
     #ppf001, ppf003, ppf005, ppf008 ppf009 = 5
     #wscepfl0060, wscepfl0078, wscepfl0080, wscepfl0081, wscepfl0082, wscepfl0086, wscepfl0089, wscepfl0096 = 7
     #reste wscepfl0087 (annotated)
-        if exp.name!="wscepfl0099" and exp.name!="wscepfl0100":
+        if exp.name!="bleb003":
             continue
 
 
