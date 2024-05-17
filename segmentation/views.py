50caa4570dd7400be68c8379bd09485cc03556ed
Clement Helsens <clementhelsens>
2024-05-17 14:56:39 +0200

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -681,15 +681,15 @@ def build_ROIs_loop():
     #ppf001, ppf003, ppf005, ppf008 ppf009 = 5
     #wscepfl0060, wscepfl0078, wscepfl0080, wscepfl0081, wscepfl0082, wscepfl0086, wscepfl0089, wscepfl0096 = 7
     #reste wscepfl0087 (annotated)
-        if exp.name!="bleb003":
-            continue
+        #if exp.name!="wscepfl0101":
+        #    continue
 
 
         experimentaldataset = ExperimentalDataset.objects.select_related().filter(experiment = exp)
         for expds in experimentaldataset:
             samples = Sample.objects.select_related().filter(experimental_dataset = expds)
             for s in samples:
-                build_ROIs(sample=s, force=True)
+                build_ROIs(sample=s, force=False)
                 
 #___________________________________________________________________________________________
 def build_ROIs(sample=None, force=False):
