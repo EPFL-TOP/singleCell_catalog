58e6e83dd14820551155d7a7a4ece83472c0177d
Clement Helsens <clementhelsens>
2024-06-07 10:45:37 +0200

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -3914,12 +3914,16 @@ def summary_handler(doc: bokeh.document.Document) -> None:
             print('intensity_mean=',intensity_mean)
             print('intensity_std =',intensity_std)
             print('intensity_sum =',intensity_sum)
-            sorted_lists  = sorted(zip(time, intensity_max, intensity_mean)) 
-            sorted_lists2 = sorted(zip(time, intensity_sum)) 
-            sorted_lists3 = sorted(zip(time, intensity_std)) 
-            time_sorted, intensity_std_sorted = zip(*sorted_lists3)
-            time_sorted, intensity_sum_sorted= zip(*sorted_lists2)
-            time_sorted, intensity_max_sorted, intensity_mean_sorted = zip(*sorted_lists)
+            sorted_lists1  = sorted(zip(time, intensity_max)) 
+            sorted_lists2  = sorted(zip(time, intensity_mean)) 
+            sorted_lists3 = sorted(zip(time,  intensity_sum)) 
+            sorted_lists4 = sorted(zip(time,  intensity_std)) 
+
+            time_sorted, intensity_max_sorted  = zip(*sorted_lists1)
+            time_sorted, intensity_mean_sorted = zip(*sorted_lists2)
+            time_sorted, intensity_sum_sorted  = zip(*sorted_lists3)
+            time_sorted, intensity_std_sorted  = zip(*sorted_lists4)
+
             intensity_traces[sample][cellID.name]["ROI"]["time"]           = time_sorted
             intensity_traces[sample][cellID.name]["ROI"]["intensity_max"]  = intensity_max_sorted
             intensity_traces[sample][cellID.name]["ROI"]["intensity_mean"] = intensity_mean_sorted
