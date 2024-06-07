de7dc75c0795697bb6fe7f60fd86ce7af29cf26f
Clement Helsens <clementhelsens>
2024-06-07 11:12:15 +0200

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -3946,6 +3946,9 @@ def summary_handler(doc: bokeh.document.Document) -> None:
             int_ch3_list = []
             
             for cell in intensity_traces[selected_positons[i]]:
+                print('cell=',cell)
+                print('intensity_traces[selected_positons[i]]=',intensity_traces[selected_positons[i]])
+                
                 print(intensity_traces[selected_positons[i]][cell]['ROI'])
                 for t in range(len(intensity_traces[selected_positons[i]][cell]['ROI']['time'])):
                     time_list.append(intensity_traces[selected_positons[i]][cell]['ROI']['time'][t])
