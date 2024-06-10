1a97dc17aa5c476d3c5209b133c371ec294f0ad1
Clement Helsens <clementhelsens>
2024-06-10 21:11:06 +0200

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -3822,7 +3822,9 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
     plot_oscillation_cycle  = bokeh.plotting.figure(title="Osc Cycle", x_axis_label='cycle number', y_axis_label='Period [min]',width=500, height=400)
     plot_oscillation_cycle.scatter(x=bokeh.transform.jitter('cycle', width=0.2, range=plot_oscillation_cycle.x_range), y='time', source=source_osc_period, size=8)
     whisker = bokeh.models.Whisker(base=bokeh.transform.jitter('cycle', width=0.2, range=plot_oscillation_cycle.x_range),
-                                   upper='time', lower='time', upper_head=None, lower_head=None, source=source_osc_period, dimension='height')
+                                   upper='time', lower='time', upper_head=None, lower_head=None, source=source_osc_period_err, dimension='height')
+    whisker.upper_head.size=20
+    whisker.lower_head.size=20
     plot_oscillation_cycle.add_layout(whisker)
 
     # Sample data
