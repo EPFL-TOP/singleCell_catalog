1f408053c2485085a722cec16d2e0ead522f01d7
Clement Helsens <clementhelsens>
2024-06-12 18:14:36 +0200

    generic update commit

--- training.py
+++ training.py
@@ -32,8 +32,13 @@ def pad_image(image, target_size):
     if delta_h % 2 != 0:
         padding = ((pad_height, pad_height+1), (pad_width, pad_width))
     
+
+    padding = ((1, 1), (1, 1))
+
     print(padding)
+
     print(image)
+
     # Pad the image
     padded_image = np.pad(image, padding, mode='constant', constant_values=0)
     print(padded_image)
