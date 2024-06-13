9a32334803fb54d2b1a8a731f7450216f17e83b0
Clement Helsens <clementhelsens>
2024-06-13 10:56:40 +0200

    generic update commit

--- segmentation/views.py
+++ segmentation/views.py
@@ -2389,7 +2389,7 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
                 hist, edges = np.histogram(img_diff.flatten(), bins=30)
                 
                 source_test_dead.data=dict(top=hist, left=edges[:-1], right=edges[1:])
-                print(source_test_dead)
+                print(source_test_dead.data)
     #___________________________________________________________________________________________
 
 
