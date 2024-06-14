017eb517a34914eaec764f4cf424cc029ee41506
Clement Helsens <clementhelsens>
2024-06-14 10:39:29 +0200

    generic update commit

--- training.py
+++ training.py
@@ -129,7 +129,7 @@ data_augmentation = tf.keras.Sequential([
 model = models.Sequential([
     data_augmentation,
 
-    layers.Input(shape=(150, 150, 1)),
+    #layers.Input(shape=(150, 150, 1)),
     layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu'),
