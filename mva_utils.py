1a39f50ada7629781e40a2546ff23efcafe6ef65
Clement Helsens <clementhelsens>
2024-06-18 14:06:47 +0200

    generic update commit

--- mva_utils.py
+++ mva_utils.py
@@ -25,15 +25,22 @@ model_simple = models.Sequential([
     data_augmentation_simple,
 
     layers.Input(shape=(150, 150, 1)),
+    
     layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
+
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
+
     layers.Conv2D(128, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
+
     layers.Flatten(),
+
     layers.Dense(512, activation='relu'),
+
     layers.Dropout(0.5),
+
     layers.Dense(1, activation='sigmoid')
 ])
 
