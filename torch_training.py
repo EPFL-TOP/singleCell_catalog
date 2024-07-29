ea9bbdb7ee64db3d8780dd6dea3199938c948559
Clement Helsens <clementhelsens>
2024-07-29 12:18:25 +0200

    add env windows

--- torch_training.py
+++ torch_training.py
@@ -2,8 +2,18 @@ import os
 import torch
 import json
 import numpy as np
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, random_split
 from torchvision import transforms
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+import torch.optim as optim
+
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
 
 class CellDataset(Dataset):
     def __init__(self, base_path, transform=None):
@@ -68,24 +78,29 @@ transform = transforms.Compose([
 
 base_path = r'D:\single_cells\training_cell_detection'
 
-train_dataset = CellDataset(base_path=base_path, transform=transform)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+
+dataset = CellDataset(base_path=base_path, transform=transform)
+# Split dataset into training and validation sets
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+batch_size = 16  # Adjust this based on your GPU memory
+train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+val_loader   = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
 
 
+# Print the number of samples in training and validation sets
+print(f'Number of training samples: {train_size}')
+print(f'Number of validation samples: {val_size}')
 
-
-
-
-import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights='FasterRCNN_ResNet50_FPN_Weights.DEFAULT')
 in_features = model.roi_heads.box_predictor.cls_score.in_features
 num_classes = 2  # 1 class (cell) + background
 model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
 
-import torch.optim as optim
+
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 model.to(device)
@@ -94,11 +109,14 @@ params = [p for p in model.parameters() if p.requires_grad]
 optimizer = optim.Adam(params, lr=1e-4)
 
 num_epochs = 25
+model_save_path = 'cell_detection_model.pth'
 
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
-    for images, targets in train_loader:
+    print(f'Starting epoch {epoch + 1}/{num_epochs}')
+
+    for i, (images, targets) in enumerate(train_loader):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
@@ -110,6 +128,58 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         running_loss += losses.item()
+        if i % 10 == 0:
+            print(f'[{epoch + 1}, {i}] loss: {losses.item():.4f}')
 
     epoch_loss = running_loss / len(train_loader)
-    print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}')
+    print(f'Epoch {epoch + 1} completed. Loss: {epoch_loss:.4f}')
+
+    torch.save(model.state_dict(), model_save_path)
+    print(f'Model saved to {model_save_path} after epoch {epoch + 1}')
+
+torch.save(model.state_dict(), model_save_path)
+print(f'Training complete. Final model saved to {model_save_path}')
+
+def predict(model, image_array):
+    model.eval()
+    transform = transforms.Compose([
+        ToTensorNormalize(),
+        transforms.Resize((512, 512))
+    ])
+    image = transform(image_array).unsqueeze(0).to(device)
+    
+    with torch.no_grad():
+        prediction = model(image)
+        
+    return prediction
+
+
+
+def plot_predictions(image, prediction):
+    fig, ax = plt.subplots(1)
+    ax.imshow(image, cmap='gray')
+    
+    for box in prediction[0]['boxes']:
+        x_min, y_min, x_max, y_max = box
+        rect = patches.Rectangle((x_min, y_min), x_max - x_min, y_max - y_min, linewidth=2, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+    
+    plt.show()
+
+model.eval()
+for i, (images, targets) in enumerate(val_loader):
+    images = list(image.to(device) for image in images)
+    targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+    
+    with torch.no_grad():
+        predictions = model(images)
+    
+    for j in range(len(images)):
+        image = images[j].cpu().numpy().squeeze(0)
+        plot_predictions(image, predictions)
+        if j == 2:
+            break
+    break
+
+
+
