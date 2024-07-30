8757f70d813265159be09d1773fcd71d2add1b67
Clement Helsens <clementhelsens>
2024-07-30 13:47:03 +0200

    add env windows

--- yolo_train.py
+++ yolo_train.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")  # Load a pretrained model
+
 results = model.train(data="C:/Users/helsens/software/singleCell_catalog/data.yaml", epochs=100, imgsz=512)
