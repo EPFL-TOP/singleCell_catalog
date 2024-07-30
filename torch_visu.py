2d83a3828bbda03b7be9908f0ea8269f9476aae2
Clement Helsens <clementhelsens>
2024-07-30 14:32:22 +0200

    add env windows

--- torch_visu.py
+++ torch_visu.py
@@ -1,5 +1,5 @@
 import torch
-import json, os, random
+import json, os, random, time
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,61 +34,74 @@ def preprocess_image(image_array):
     image = transform(image_array)
     return image.unsqueeze(0)  # Add batch dimension
 
-def visualize_predictions(image, predictions, boxes):
-    fig, ax = plt.subplots(1)
-    img = image.squeeze(0).squeeze(0).cpu().numpy()
-    ax.imshow(img, cmap='gray')
-    for box in predictions[0]['boxes']:
-        x_min, y_min, x_max, y_max = box.cpu().numpy()
-        rect = patches.Rectangle((x_min, y_min), x_max - x_min, y_max - y_min, linewidth=1, edgecolor='r', facecolor='none')
-        ax.add_patch(rect)
-    
-    for box in boxes:
-        rect = patches.Rectangle((box[0], box[2]), box[1] - box[0], box[3] - box[2], linewidth=1, edgecolor='white', facecolor='none')
-        ax.add_patch(rect)
-    plt.show()
+def visualize_predictions(images, predictions, boxes):
+    for idx, (image, pred, box) in enumerate(zip(image, predictions, boxes)):
+        fig, ax = plt.subplots(1)
+        img = image.squeeze(0).squeeze(0).cpu().numpy()
+        ax.imshow(img, cmap='gray')
+        for p in pred[0]['boxes']:
+            x_min, y_min, x_max, y_max = p.cpu().numpy()
+            rect = patches.Rectangle((x_min, y_min), x_max - x_min, y_max - y_min, linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+        
+        for b in box:
+            rect = patches.Rectangle((b[0], b[2]), b[1] - b[0], b[3] - b[2], linewidth=1, edgecolor='white', facecolor='none')
+            ax.add_patch(rect)
+        plt.show()
 
 def main():
+
+    # Load the model
+    start_time = time.time()
     model_path = 'cell_detection_model.pth'
     num_classes = 2
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
     # Load the model
     model = load_model(model_path, num_classes, device)
 
+    end_time = time.time()
+    print(f"Loading model took {end_time - start_time:.4f} seconds")
+
+
     json_path = r'D:\single_cells\training_cell_detection\wscepfl0080\wscepfl0080_well2\wscepfl0080_xy41\frame0.json'
     base_path = r'D:\single_cells\training_cell_detection\wscepfl0080'
-
+    base_path = r'D:\single_cells\training_cell_detection\wscepfl0060\wscepfl0060_well1'
     json_files = []
     for root, _, files in os.walk(base_path):
         for file in files:
             if file.endswith('.json'):
                 
-                if 'xy07' not in root:continue
+                if 'xy04' not in root:continue
                 #if 'xy40' not in root:continue
                 json_files.append(os.path.join(root, file))
 
-    random.shuffle(json_files)
-    nimages=100
+
+    processed_images=[]
+    boxes=[]
+    #random.shuffle(json_files)
     for idx in range(len(json_files)):
         print(json_files[idx])
-        if idx>=nimages:break
         json_path=json_files[idx]
         with open(json_path, 'r') as f:
             data = json.load(f)
         image_array = np.array(data['data'], dtype=np.float32)  # Convert to float32 to avoid overflow
-        boxes = [ann['bbox'] for ann in data['annotations']]
-
+        boxes.append( [ann['bbox'] for ann in data['annotations']])
 
         # Preprocess the image
         image = preprocess_image(image_array).to(device)
+        processed_images.append(image)
 
-        # Make predictions
-        with torch.no_grad():
-            predictions = model(image)
+    # Convert list of images to a batch tensor
+    batch_images = torch.stack(processed_images).to('cuda')
+    # Make predictions
+    start_time = time.time()
+    with torch.no_grad():
+        predictions = model(image)
+    end_time = time.time()
+    print(f"Inference on batch size {len(batch_images)} took {end_time - start_time:.4f} seconds")
 
-        # Visualize the predictions
-        visualize_predictions(image.cpu(), predictions, boxes)
+    # Visualize the predictions
+    visualize_predictions(processed_images, predictions, boxes)
 
 if __name__ == "__main__":
     main()
