539194a1ffeffd5a140f1189b4a7d334bb22f801
Clement Helsens <clementhelsens>
2024-07-30 14:41:11 +0200

    add env windows

--- torch_visu.py
+++ torch_visu.py
@@ -35,7 +35,9 @@ def preprocess_image(image_array):
     return image.unsqueeze(0)  # Add batch dimension
 
 def visualize_predictions(images, predictions, boxes):
+    print('images, predictions, boxes ',images'  ', predictions,'  ', boxes)
     for idx, (image, pred, box) in enumerate(zip(images, predictions, boxes)):
+
         fig, ax = plt.subplots(1)
         img = image.squeeze(0).squeeze(0).cpu().numpy()
         ax.imshow(img, cmap='gray')
