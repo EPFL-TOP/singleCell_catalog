b947a25b0204a1545b2e6a855e4e743cf3b36b91
Clement Helsens <clementhelsens>
2024-08-12 13:53:27 +0200

    add env windows

--- segmentation/views.py
+++ segmentation/views.py
@@ -4492,6 +4492,16 @@ def phenocheck_handler(doc: bokeh.document.Document) -> None:
     for t in threads: t.join()
 
 
+
+    #___________________________________________________________________________________________
+    def select_cell_type_callback(attr, old, new):
+        print('select_cell_type_callback attr, old, new=',attr,old,new)
+        new_layout = create_plots_layout()
+        doc.clear()  # Clear the current document
+        #doc.add_root(bokeh.layouts.column(select_cell_type, new_layout))
+    select_cell_type.on_change('value', select_cell_type_callback)
+
+
     #___________________________________________________________________________________________
     def create_plots_layout():
         plots = []
@@ -4548,14 +4558,6 @@ def phenocheck_handler(doc: bokeh.document.Document) -> None:
         return layout
 
 
-
-    #___________________________________________________________________________________________
-    def select_cell_type_callback(attr, old, new):
-        print('select_cell_type_callback attr, old, new=',attr,old,new)
-        new_layout = create_plots_layout()
-        doc.clear()  # Clear the current document
-        #doc.add_root(bokeh.layouts.column(select_cell_type, new_layout))
-    select_cell_type.on_change('value', select_cell_type_callback)
 
     # Create the initial layout
     layout = create_plots_layout()
