c478c4f4090ee30e12c17c26d30dc8984faee7d2
Clement Helsens <clementhelsens>
2024-08-30 14:27:00 +0200

    masks images in memory

--- segmentation/views.py
+++ segmentation/views.py
@@ -1642,10 +1642,10 @@ def segmentation_handler(doc: bokeh.document.Document) -> None:
 
                     f = open(os.path.join(NASRCP_MOUNT_POINT, seg.file_name))
                     data = json.load(f)
-                    mask1=np.ones(ind_images_list_norm[frame.number].shape, dtype=bool)
+                    mask1=np.ones(ind_images_list_norm[frame.number][0].shape, dtype=bool)
                     mask1=mask1*255
                     print('mask shape ', mask1.shape)
-                    print('ind_images_list_norm[frame.number] ',ind_images_list_norm[frame.number].shape)
+                    print('ind_images_list_norm[frame.number] ',ind_images_list_norm[frame.number][0].shape)
                     for i in range(data['npixels']):
                         mask1[frame.height-data['x'][i]][data['y'][i]]=ind_images_list_norm[0][frame.number][frame.height-data['x'][i]][data['y'][i]] 
                     out_dict[roi.cell_id.name][seg][frame.number] = mask1
